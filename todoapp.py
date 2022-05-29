d23aaf4a2cbdced8ed58b64e9a45fbf1fae137c3
mercyarrobov <myarrobo@espe.edu.ec>
2022-05-28 21:18:47 -0500

    Archivo Final

--- todoapp.py
+++ todoapp.py
@@ -1,32 +1,46 @@
 
 #Importar Flask
-from flask import Flask, render_template
-#Instanciar a la aplicación
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect
+# Instancia de Flask. Aplicación
+app = Flask(__name__, template_folder='template')
+
 #Lista dónde se almacenará las tareas pendientes
-lista = []
+lista= []
 
 #Define la ruta principal
 @app.route('/')
 
 #Función para llamar a la página index.html
 def index():
-    return render_template()
+	return render_template("index.html", lista=lista)
 
-#Define la ruta enviar
-@app.route('/enviar')
+#Define la ruta enviar y para que acepte las peticiones POST.
+@app.route('/enviar', methods=[ 'POST'])
 
 #Función para enviar la lista
 def enviar():
-    return
+	if request.method == 'POST':
+		#Obtenemos el valor de los campos tarea
+		# Esto lo hacemos con el diccionario "form"
+		tarea = request.form.get('tareaI')
+		lista.append(tarea)
+		# Retornamos la respuesta a la página index
+		return redirect('/')
 
-#Define la ruta borrar
-@app.route('/borrar')
+#Define la ruta borrar, y para que acepte las peticiones POST.
+@app.route("/borrar", methods=['POST'])
 
 #Función para borrar la lista
 def borrar():
-    return
+    if request.method == 'POST':
+		#Obtenemos el valor de los campos tarea
+		# Esto lo hacemos con el diccionario "form"
+        tarea = request.form.get('tareaB')
+        lista.remove(tarea) 
+		# Retornamos la respuesta a la página index
+        return redirect('/')
 
 #main del programa 
 if __name__ == "__main__":
+	# Iniciamos la apicación en modo debug
 	app.run(debug=True)
